5fa25d4e54fa4009f38e0b57fc95fd348369583f
veera <veeramaniamk@gmail.com>
2026-07-01 18:48:42 +0530

    first commit

---
+++ src/test/java/com/veera/firebase/FirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veera.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
